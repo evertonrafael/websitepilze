d062600823c61c0d5a94411449c6e34ef026a6e0
Rafael Farias <evertonrafael00@gmail.com>
2017-07-24 23:07:02 -0300

    base

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+var gulp        = require('gulp');
+var bower       = require('gulp-bower');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+var minify      = require('gulp-minify');
+var cssmin      = require('gulp-cssmin');
+var rename      = require('gulp-rename');
+
+gulp.task('bower', function() {
+  return bower();
+});
+
+var sassOptions = {
+    errLogToConsole: true,
+    outputStyle: 'expanded',
+    includePaths: ['lib/bootstrap-sass/assets/stylesheets', 'lib/font-awesome/scss']
+};
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass', 'compress'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("lib/sass/*.scss", ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+    gulp.watch("lib/css/*.css").on('change', browserSync.reload);
+    gulp.watch("lib/js/*.js").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("lib/**/*.scss")
+        .pipe(sass(sassOptions))
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest("lib/css/min"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('compress', function() {
+  gulp.src('lib/js/*.js')
+    .pipe(minify({
+        ext:{
+            src:'-debug.js',
+            min:'.min.js'
+        },
+        exclude: ['tasks'],
+        ignoreFiles: ['.all.js', '-min.js']
+    }))
+    .pipe(gulp.dest('lib/js/min'))
+});
+
+gulp.task('default', ['serve', 'bower']);
