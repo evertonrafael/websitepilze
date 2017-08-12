1f87e902ff0450ea04be46b06ac6255a8088d5fc
Rafael Farias <evertonrafael00@gmail.com>
2017-08-12 18:41:41 -0300

    correcoes gulpjs

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var bower       = require('gulp-bower');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
 var uglify      = require('gulp-uglify');
+var minify      = require('gulp-minify');
+var concat      = require('gulp-concat');
 var pump        = require('pump');
 var cssmin      = require('gulp-cssmin');
 var concatCss   = require('gulp-concat-css');
@@ -19,16 +21,17 @@ var sassOptions = {
 };
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass', 'compress'], function() {
+gulp.task('serve', ['sass', 'scripts'], function() {
 
     browserSync.init({
         server: "./"
     });
 
     gulp.watch("lib/sass/*.scss", ['sass']);
+    gulp.watch("lib/js/**/*.js", ['scripts']);
     gulp.watch("*.html").on('change', browserSync.reload);
     gulp.watch("lib/css/*.css").on('change', browserSync.reload);
-    gulp.watch("lib/js/**/*.js").on('change', browserSync.reload);
+    gulp.watch("lib/js/*.js").on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
@@ -42,14 +45,18 @@ gulp.task('sass', function() {
         .pipe(browserSync.stream());
 });
 
-gulp.task('compress', function (cb) {
-  pump([
-        gulp.src('lib/js/*.js'),
-        uglify(),
-        gulp.dest('lib/js/min')
-    ],
-    cb
-  );
+//script paths
+var jsFiles = 'lib/js/*.js',
+jsDest = 'lib/js/min';
+
+gulp.task('scripts', function() {
+return gulp.src(jsFiles)
+    .pipe(concat('scripts.js'))
+    .pipe(gulp.dest(jsDest))
+    .pipe(rename('scripts.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(jsDest))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('default', ['serve', 'bower']);
